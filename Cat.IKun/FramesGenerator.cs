dbbedf7c1531d5d945e8a33a2f33ad92b51a55b7
猫猫球 <45891684+FanSun01@users.noreply.github.com>
2022-11-03 22:40:20 +0800

    'Init'

---
+++ Cat.IKun/FramesGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat.IKun
+{
+    internal class FramesGenerator
+    {
+        public IEnumerable<Bitmap> GetFrames()
+        {
+            using var archive = new ZipArchive(new MemoryStream(Properties.Resources.frame), ZipArchiveMode.Read);
+            int frameNumber = 1;
+            while (true)
+            {
+                var frame = archive.GetEntry($"frame{frameNumber:0000}.jpg");
+                ++frameNumber;
+                if (frame is not null)
+                {
+                    using var stream = frame.Open();
+                    yield return new Bitmap(stream);
+                }
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ Cat.IKun/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+namespace Cat.IKun
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var player = new Player();
+            var framesGenerator = new FramesGenerator();
+            var fps = 30;
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    switch (args[i].ToLowerInvariant())
+                    {
+                        case "--fps":
+                            _ = int.TryParse(args[++i], out fps);
+                            break;
+
+                        default:
+                            Console.WriteLine(@"
+Usage :
+ikun [options]
+
+Options :
+--fps <frames per second>           Overrides the 30 fps limit");
+                            return;
+                    }
+                }
+            }
+
+            var files = framesGenerator.GetFrames();
+            player.Play(files, fps);
+        }
+    }
+}
